1d20e2379b3a57847d24e47d2b4d4df66efee245
Sharon Lake <parallaxplace@gmail.com>
2017-12-06 21:18:37 +0000

    x-options view & remove kb header from application

--- hs/views.py
+++ hs/views.py
@@ -1,9 +1,12 @@
 from django import forms
 from .forms import HomeschoolApplicationModelForm
 from django.shortcuts import render, redirect
+from django.views.decorators.clickjacking import xframe_options_exempt
+
 
 # Create your views here.
 
+@xframe_options_exempt
 def application_upload(request):
 	if request.method == "POST":
 		form = HomeschoolApplicationModelForm(request.POST)
